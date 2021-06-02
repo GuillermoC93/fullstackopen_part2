a27e026e421d7dd813ea409a73e484d284e065bd
Guillermo <guillermoc93@protonmail.com>
2021-06-01 22:09:38 -0500

    implemented search function

---
+++ countries/src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Search = (props) => {
+  return (
+    <form>
+      <div>
+        find countries:
+        <input
+          value={props.filter}
+          onChange={props.handleFilterChange}
+        />
+        <ul>
+          {props.filtered.map(country => <li key={country.name}>{country.name}</li>)}
+        </ul>
+      </div>
+    </form>
+  )
+}
+
+const DisplayCountry = (props) => {
+  return (
+    <h2>{props.country}</h2>
+  )
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([])
+  const [filter, setFilter] = useState('')
+  const [filtered, setFiltered] = useState([])
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+
+    if (event.target.value === '') {
+      setFiltered([])
+      return
+    }
+    
+    const copy = [...countries]
+    setFiltered(copy.filter(country => country.name.toLowerCase().includes(event.target.value.toLowerCase())))
+  }
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  return (
+    <div>
+      <Search filter={filter} filtered={filtered} handleFilterChange={handleFilterChange}/>
+    </div>
+  );
+}
+
+export default App;
